56fca52b8ca10f754581058d7fb89dc87f6f710a
Kasmik004 <67088222+Kasmik004@users.noreply.github.com>
2024-03-04 00:20:59 +0545

    Spotipy done, youtube start

--- spo.py
+++ spo.py
@@ -6,73 +6,15 @@ import json
 import os
 import dotenv
 import webbrowser
+import requests
 
-from flask import Flask, redirect, session, url_for, request
+from flask import Flask, redirect, session, url_for, request, render_template
 dotenv.load_dotenv()
 
 client_id = os.getenv("SPOTIPY_CLIENT_ID")
 client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 redirect_uri = 'http://127.0.0.1:5000/callback'
 
-# app = Flask(__name__)
-# app.secret_key = 'dfhkdsjhfdskjkasdf'
-
-# @app.route("/")
-# def home():
-#     auth_url = create_auth().get_authorize_url()
-#     return redirect(auth_url)
-
-# @app.route("/redirect")
-# def redirect():
-#     session.clear
-#     code = request.args.get('code')
-#     token_info = create_auth().get_access_token(code)
-#     session['TOKEN_INFO'] = token_info
-#     return redirect(url_for('save'), external=True)    
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-# def create_auth():
-#     scope= 'user-library-read, playlist-modify-public, playlist-modify-private'
-#     username = '31tmrxnxrdyyiuuay4cquq6dzpty'
-#     redirect_url = url_for('redirect')
-#     token = SpotifyOAuth(scope=scope, username=username,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_url)
-#     #spotipyObj = spotipy.Spotify(auth_manager=token)
-#     return token
-
-# def get_token():
-#     token_info = session.get('TOKEN_INFO', None)
-#     if not token_info:
-#         return redirect(url_for('hone',external=False))
-    
-#     now = int(time.time())
-#     is_expired = get_token['expires_in'] - now <60
-#     if(is_expired): 
-#         spotify_oauth = create_spotify_oauth()
-
-
-# token = SpotifyOAuth(scope=scope, username=username,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri)
-# spotipyObj = spotipy.Spotify(auth_manager=token)
-# print("URL:", (token.get_authorize_url()))
-# url_ = token.get_authorize_url()
-
-
-# #create the playlist
-# playist_name= input("Enter a name for your new playlist: ")
-# playist_description = input("Enter a description for your new playlist: ")
-
-
-# spotipyObj.user_playlist_create(user=username,name=playist_name,public=True, description=playist_description)
-
-# # user_input = input("enter the song: ")
-# # list_of_songs = []
-
-# # while user_input != 'quit':
-# #     result = spotipyObj.search(q=user_input)
-# #     print(json.dumps(result,sort_keys=4,indent=4))
-# #     user_input = input("Enter the song: ")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
@@ -92,14 +34,52 @@ auth_manager = SpotifyOAuth(
 
 sp = Spotify(auth_manager=auth_manager)
 
-@app.route('/')
+
+@app.route('/youtube')
+def youtube():
+    
+
+    api_key = 'AIzaSyDtOr1WM1s2X5oy_RmULTQGUdPL0L4PzK8'
+    playlist_url = 'https://www.googleapis.com/youtube/v3/playlistItems'
+    prams = {
+        'key' : api_key,
+        'part' : "snippet",
+        'maxResults':  10,
+        'playlistId' : "PLUWT5vnZ7a39F4DulEif3X3vCXRv6KqMt"
+    }
+
+    results = requests.get(playlist_url, params=prams)
+
+    titles = []
+    for result in results.json()['items']:
+        titles.append(result['snippet']['title'])
+
+    return titles
+
+
+
+
+@app.route('/index')
+def index():
+    session['index'] = 1
+    return render_template("index.html")
+
+@app.route('/',methods=['GET','POST'])
 def home():
+    if 'index' not in session:
+        return redirect(url_for('index'))
+
+    session['playlist_name'] = request.form['name']
+    session['playlist_description'] = request.form['des']
     cached_token = cache_handler.get_cached_token()
     if not auth_manager.validate_token(cached_token):
         auth_url = auth_manager.get_authorize_url()
         return redirect(auth_url)
     
     return redirect(url_for('get_playlists'))
+
+
+
 
 @app.route('/callback')
 def callback():
@@ -114,10 +94,30 @@ def get_playlists():
         auth_url = auth_manager.get_authorize_url()
         return redirect(auth_url)
     
+    username = sp.current_user()['id']
+    # playist_name="default"
+    # playist_description="default"
+    
+    # playist_name= session['playlist_name']
+    # playist_description = session['playlist_description']
+    
+
+    #sp.user_playlist_create(user=username,name=playist_name,public=True, description=playist_description)
+    #return "Success"
+    
     playlist = sp.current_user_playlists()
+    #return playlist
     playlist_list = [(pl['name'],pl['external_urls']['spotify']) for pl in playlist['items']]
-    playlist_html = '<br'.join([f'{name}: {url}' for name,url in playlist_list])
+    
+    playlist_html = '<br>'.join([f'{name}: {url}' for name,url in playlist_list])
     return playlist_html
+
+@app.route('/add_playlist')
+def add_playlist():
+    username = sp.current_user()
+    playist_name= input("Enter a name for your new playlist: ")
+    playist_description = input("Enter a description for your new playlist: ")
+    return None
 
 @app.route('/logout')
 def logout():
---
+++ youtube.py
@@ -0,0 +1,4 @@
+
+
+
+api_key = 'AIzaSyDtOr1WM1s2X5oy_RmULTQGUdPL0L4PzK8'
