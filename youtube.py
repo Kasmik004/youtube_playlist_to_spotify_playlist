99b08b7d31ca351b8a3d9f51ab0d7bbdc9100b3a
Kasmik004 <67088222+Kasmik004@users.noreply.github.com>
2024-03-04 17:05:11 +0545

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,4 +1,27 @@
 
-
-
+import requests
+import youtube_title_parse
 api_key = 'AIzaSyDtOr1WM1s2X5oy_RmULTQGUdPL0L4PzK8'
+playlist_url = 'https://www.googleapis.com/youtube/v3/playlistItems'
+prams = {
+    'key' : api_key,
+    'part' : "snippet",
+    'maxResults':  10,
+    'playlistId' : "PLUWT5vnZ7a39uMBO5bPkTtjxW8f_RY0Ti"
+    }
+
+results = requests.get(playlist_url, params=prams)
+
+titles = []
+for result in results.json()['items']:
+    titles.append(result['snippet']['title'])
+
+art =[]
+song =[]
+for t in titles:
+    temp_a, temp_b = youtube_title_parse.get_artist_title(t)
+    art.append(temp_a)
+    song.append(temp_b)
+
+for s in art:
+    print(s)
